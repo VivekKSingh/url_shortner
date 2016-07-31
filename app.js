a677de387631db2af389957a787473dde1f081ad
Vivek <svivekkumar88@gmail.com>
2016-07-31 19:08:13 -0400

    Removing public directory since its no longer required.

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var app = express();
 app.set('port', process.env.PORT || config.webport);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname +  '/public'));
 
 var routes = require("./routes/routes.js")(app, utils);
 var database = require("./libs/db.js");
